3215b154d94c1c32964381122c3b8f5711a05779
Maximus7410 <33791590+Maximus7410@users.noreply.github.com>
2019-10-08 16:33:57 -0700

    Checkpoint #1

--- app/src/main/java/edu/up/cs301/pig/PigGameState.java
+++ app/src/main/java/edu/up/cs301/pig/PigGameState.java
@@ -4,4 +4,22 @@ import edu.up.cs301.game.infoMsg.GameState;
 
 public class PigGameState extends GameState
 {
+
+	public PigGameState()
+	{
+		this.turnID = 0;
+		this.playerZeroScore = 0;
+		this.playerOneScore = 0;
+		this.runningTotal = 0;
+		this.diceValue = 0;
+	}
+
+	public PigGameState(PigGameState that)
+	{
+		this.turnID = that.turnID;
+		this.playerZeroScore = that.playerZeroScore;
+		this.playerOneScore = that.playerOneScore;
+		this.runningTotal = that.runningTotal;
+		this.diceValue = that.diceValue;
+	}
 }
